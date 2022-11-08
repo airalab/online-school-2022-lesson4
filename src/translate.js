68643003183d1b09faf9b43fc2c7a0bff749c598
vol4tim <vol4tim@gmail.com>
2022-11-08 15:22:42 +0400

    add language support

---
+++ src/translate.js
@@ -0,0 +1,81 @@
+export default {
+  en: {
+    header: {
+      title: "Lesson 4 Test",
+      link: "Introduction Course: Lesson 4"
+    },
+    main: {
+      title: "Dear participant!",
+      text1:
+        "You are invited to take this test for Lesson 4, which is dedicated to the second chapter of the Robonomics Whitepaper 2022 “Robonomics Architecture” and its section on IoT systems. The test is designed to encourage you to reflect on the ideas covered in the lesson, and you will not find answers to some questions directly in the whitepaper. You will need to think about them by yourself, so be careful.",
+      text2:
+        "To pass the test, you need to score 7 correct answers out of 11 (60%, the standard value in academic practice).",
+      wait: "Please wait"
+    },
+    account: {
+      title: "1. Your account",
+      notice: "[Robonomics parachain format]",
+      copy: "Copy address",
+      copied: "Address copied!",
+      create: {
+        text: "Create or activate your account in {link}",
+        link: "Polkadot.js extension"
+      }
+    },
+    sender: {
+      title: "2. Your answers",
+      subtitle: "Your answers has been sent",
+      explorer: "View tx explorer"
+    },
+    quiz: {
+      question: "Question",
+      wait: "Please wait",
+      sign: "Sign and send",
+      subscription: {
+        text: "You need free IoT subscription to make a transaction.Check out the academy-faucet bot on the {link} server.",
+        link: "Discord"
+      },
+      error: "all questions must be answered"
+    }
+  },
+
+  ru: {
+    header: {
+      title: "Урок 4 Тест",
+      link: "Вводный курс: Урок 4"
+    },
+    main: {
+      title: "Уважаемый участник!",
+      text1:
+        "Вам предлагается пройти тест к четвертому уроку о второй главе Robonomics Whitepaper 2022 под названием «Архитектура Робономики», и в частности разделу главы, посвященному IoT-системам. Тест предназначен для того, чтобы побудить вас задуматься над идеями, затронутыми в главе. Не все ответы на некоторые вопросы можно просто найти в тексте. Вам придется самостоятельно разобраться в них, поэтому будьте внимательны.",
+      text2:
+        "Для прохождения теста необходимо набрать 7 из 11 правильных ответов (порог в 60%, стандартный порог в академической практике).",
+      wait: "Пожалуйста подождите"
+    },
+    account: {
+      title: "1. Ваш аккаунт",
+      notice: "[В формате парачейна Робономики]",
+      copy: "Скопировать адрес",
+      copied: "Адрес скопирован!",
+      create: {
+        text: "Создайте или активируйте свой аккаунт в {link}",
+        link: "расширении Polkadot.js"
+      }
+    },
+    sender: {
+      title: "2. Ваши ответы",
+      subtitle: "Ваши ответы отправлены",
+      explorer: "Просмотр транзакции через проводник"
+    },
+    quiz: {
+      question: "Вопрос",
+      wait: "Пожалуйста подождите",
+      sign: "Подписать и отправить",
+      subscription: {
+        text: "Вам нужна бесплатная IoT-подписка, чтобы выполнить транзакцию. Для этого проверьте бота academy-faucet на нашем {link} сервере.",
+        link: "Discord"
+      },
+      error: "для всех вопросов должны быть даны ответы"
+    }
+  }
+};
